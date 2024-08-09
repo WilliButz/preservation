2de7545ab94bcd7fdafaec280f00898556110230
WilliButz <willibutz@posteo.de>
2024-08-09 13:42:44 +0200

    tests: add comments, run nixfmt + some manual formatting changes

--- tests/basic.nix
+++ tests/basic.nix
@@ -6,82 +6,101 @@ in
 {
   name = "preservation-basic";
 
-  nodes.machine = { pkgs, utils, ... }: {
-    imports = [
-      ../module.nix
-    ];
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      # import the preservation module
+      imports = [ ../module.nix ];
 
-    testing.initrdBackdoor = true;
-    boot.initrd.systemd = {
-      enable = true;
-      extraBin = {
-        mountpoint = "${pkgs.util-linux}/bin/mountpoint";
-      };
-    };
-
-    users.users = {
-      alice = {
-        isNormalUser = true;
-        # custom home directory
-        home = "/home/wonderland";
-      };
-      butz.isNormalUser = true;
-    };
-
-    virtualisation = {
-      memorySize = 2048;
-      # separate block device for preserved state
-      emptyDiskImages = [ 23 ];
-      fileSystems."/state" = {
-        device = "/dev/vdb";
-        fsType = "ext4";
-        neededForBoot = true;
-        autoFormat = true;
-      };
-    };
-
-    preservation = {
-      enable = true;
-      preserveAt."/state" = {
-        directories = [
-          "/var/lib/someservice"
-        ];
-        files = [
-          { file = "/etc/wpa_supplicant.conf"; how = "symlink"; }
-          { file = "/etc/machine-id"; inInitrd = true; }
-        ];
-        users = {
-          alice.directories = [
-            ".rabbit_hole"
+      # module configuration
+      preservation = {
+        # global enable switch
+        enable = true;
+        # all files and directories are preserved under "/state" in this test
+        preserveAt."/state" = {
+          directories = [
+            "/var/lib/someservice"
           ];
-          butz = {
-            files = [
-              { file = ".config/foo"; mode = "0600"; }
-              "bar"
-              { file = ".symlinks/baz"; how = "symlink"; }
-            ];
-            directories = [
-              "unshaved_yaks"
-            ];
+          files = [
+            { file = "/etc/wpa_supplicant.conf"; how = "symlink"; }
+            # some files need to be prepared very early, machine-id is one such case
+            { file = "/etc/machine-id"; inInitrd = true; }
+          ];
+          # per-user configuration is possible
+          # similar to impermanence this configures ownership for the respective users
+          users = {
+            alice = {
+              directories = [
+                ".rabbit_hole"
+              ];
+            };
+            butz = {
+              files = [
+                { file = ".config/foo"; mode = "0600"; }
+                "bar"
+                # an empty file with may be created at the symlink's
+                # target, i.e. on the persistent volume
+                { file = ".symlinks/baz"; how = "symlink"; createLinkTarget = true; }
+              ];
+              directories = [
+                "unshaved_yaks"
+              ];
+            };
           };
         };
       };
-    };
-  };
 
-  testScript = { nodes, ... }:
+      # test-specific configuration below
+
+      testing.initrdBackdoor = true;
+      boot.initrd.systemd = {
+        enable = true;
+        extraBin = {
+          mountpoint = "${pkgs.util-linux}/bin/mountpoint";
+        };
+      };
+
+      users.users = {
+        alice = {
+          isNormalUser = true;
+          # custom home directory
+          home = "/home/wonderland";
+        };
+        butz.isNormalUser = true;
+      };
+
+      virtualisation = {
+        memorySize = 2048;
+        # separate block device for preserved state
+        emptyDiskImages = [ 23 ];
+        fileSystems."/state" = {
+          device = "/dev/vdb";
+          fsType = "ext4";
+          neededForBoot = true;
+          autoFormat = true;
+        };
+      };
+
+    };
+
+  testScript =
+    { nodes, ... }:
     let
       butzHome = nodes.machine.users.users.butz.home;
 
-      allFiles = lib.flatten (lib.mapAttrsToList (_: preservationLib.getAllFiles) nodes.machine.preservation.preserveAt);
-      allDirs = lib.flatten (lib.mapAttrsToList (_: preservationLib.getAllDirectories) nodes.machine.preservation.preserveAt);
+      allFiles = lib.flatten (
+        lib.mapAttrsToList (_: preservationLib.getAllFiles) nodes.machine.preservation.preserveAt
+      );
+      allDirs = lib.flatten (
+        lib.mapAttrsToList (_: preservationLib.getAllDirectories) nodes.machine.preservation.preserveAt
+      );
       initrdFiles = builtins.filter (conf: conf.inInitrd) allFiles;
       initrdDirs = builtins.filter (conf: conf.inInitrd) allDirs;
       initrdJSON = builtins.toJSON (initrdDirs ++ initrdFiles);
       allJSON = builtins.toJSON (allDirs ++ allFiles);
     in
-      # (for syntax highlighting)
-      /* python */
+    # (for syntax highlighting)
+    /* python */
     ''
       import json
 
